c74dec4f97f8cda9d99a1a4de331f5faabcbe844
Lorenzo Parra <alejandro36036@gmail.com>
2025-12-12 22:22:10 -0400

    feature: implementation of sections primary
    implementation of sections primary in page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import SectionPrimary from "./components/SectionPrimary";
+
+export default function Home() {
+  return (
+    <>
+      <main className="bg-white  min-h-screen w-full ">
+        <SectionPrimary />
+      </main>
+    </>
+  );
+}
