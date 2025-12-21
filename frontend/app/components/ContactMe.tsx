442db033668c07782b58679bcfa5ba1c161bd745
Lorenzo Parra <alejandro36036@gmail.com>
2025-12-21 14:08:26 -0400

    feature: abjust of contact me
    abjust of page contact me

--- frontend/app/components/ContactMe.tsx
+++ frontend/app/components/ContactMe.tsx
@@ -54,10 +54,10 @@ const ContactPage = () => {
                       Teléfono
                     </p>
                     <a
-                      href="tel:+3480000000"
+                      href="tel:+5804249701950"
                       className="text-xl text-blue-600 font-semibold hover:underline"
                     >
-                      +58 000 00 00 00
+                      +58 04249701950
                     </a>
                   </div>
                 </div>
@@ -74,7 +74,8 @@ const ContactPage = () => {
                     <p className="text-xl text-blue-600 font-semibold capitalize">
                       Venezuela, San Félix
                     </p>
-                    <p className="text-base text-gray-500 mt-2 font-medium">
+                    <p className="flex items-center gap-2 text-base text-green-700 mt-2 font-medium">
+                      <span className="w-2 h-2 rounded-full bg-green-400"></span>
                       Disponible para trabajo remoto
                     </p>
                   </div>
@@ -125,7 +126,7 @@ const ContactPage = () => {
                   type="text"
                   id="nombre"
                   placeholder="Tu nombre completo"
-                  className="block w-full px-5 py-4 border-gray-400 rounded-lg shadow-sm placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 sm:text-base border"
+                  className="block w-full px-5 py-4 border-gray-400 rounded-lg shadow-sm placeholder-gray-300 focus:outline-blue-500 focus:border-blue-500 sm:text-base border"
                   required
                 />
               </div>
@@ -141,7 +142,7 @@ const ContactPage = () => {
                   type="email"
                   id="email"
                   placeholder="tu@email.com"
-                  className="block w-full px-5 py-4 border-gray-400 rounded-lg shadow-sm placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 sm:text-base border"
+                  className="block w-full px-5 py-4 border-gray-400 rounded-lg shadow-sm placeholder-gray-300 focus:outline-blue-500 focus:border-blue-500 sm:text-base border"
                   required
                 />
               </div>
@@ -157,7 +158,7 @@ const ContactPage = () => {
                   type="text"
                   id="asunto"
                   placeholder="¿De qué quieres hablar?"
-                  className="block w-full px-5 py-4 border-gray-400  rounded-lg shadow-sm placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 sm:text-base border"
+                  className="block w-full px-5 py-4 border-gray-400  rounded-lg shadow-sm placeholder-gray-300 focus:outline-blue-500 focus:border-blue-500 sm:text-base border"
                   required
                 />
               </div>
@@ -173,7 +174,7 @@ const ContactPage = () => {
                   id="mensaje"
                   rows={6}
                   placeholder="Cuéntame sobre tu proyecto..."
-                  className="block w-full px-5 py-4 border-gray-400  rounded-lg shadow-sm placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 sm:text-base border resize-none"
+                  className="block w-full px-5 py-4 border-gray-400  rounded-lg shadow-sm placeholder-gray-300 focus:outline-blue-500 focus:border-blue-500 sm:text-base border resize-none"
                   required
                 ></textarea>
               </div>
@@ -181,7 +182,7 @@ const ContactPage = () => {
               <div className="pt-4">
                 <button
                   type="submit"
-                  className="w-full inline-flex items-center justify-center px-8 py-4 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-200 hover:shadow-xl"
+                  className="w-full inline-flex items-center justify-center px-8 py-4 border border-transparent rounded-lg shadow-lg text-lg font-bold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:outline-blue-500 transition-all duration-200 hover:shadow-xl"
                 >
                   Enviar Mensaje
                   <Send className="w-6 h-6 ml-3 -rotate-12" strokeWidth={2} />
