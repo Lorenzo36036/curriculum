62fbdab31ea37b1eac8e0827d36b027f41dc340e
Lorenzo Parra <alejandro36036@gmail.com>
2025-12-21 21:37:30 -0400

    fix: abjust of bugs
    abjust of bugs

--- frontend/app/components/input/FormField.tsx
+++ frontend/app/components/input/FormField.tsx
@@ -2,9 +2,9 @@
 "use client";
 import { FormFieldProps } from "@/app/interfaces/types";
 import { useEffect, useState } from "react";
+import { FieldValues } from "react-hook-form";
 
-
-const FormField: React.FC<FormFieldProps> = ({
+const FormField = <T extends FieldValues>({
   id,
   type,
   placeholder,
@@ -12,38 +12,38 @@ const FormField: React.FC<FormFieldProps> = ({
   register,
   error,
   valueAsNumber,
-}) => {
-   const [errorData, setErrorData] = useState("");
+}: FormFieldProps<T>) => {
+  const [errorData, setErrorData] = useState("");
 
   useEffect(() => {
     if (error?.message) {
       setErrorData(error.message);
     }
   }, [error?.message]);
-  
+
   return (
     <div>
-    <input
-      id={id}
-      className=" bg-gray-50 border border-gray-300 focus:outline-blue-500 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
-      type={type}
-      placeholder={placeholder}
-      {...register(name, { valueAsNumber })}
+      <input
+        id={id}
+        className=" bg-gray-50 border border-gray-300 focus:outline-blue-500 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+        type={type}
+        placeholder={placeholder}
+        {...register(name, { valueAsNumber })}
       />
-    <span
-      className={`ease-in-out absolute duration-300 transition-all  text-red-600
+      <span
+        className={`ease-in-out absolute duration-300 transition-all  text-red-600
         
-        ${error 
-        ? "opacity-100 translate-y-0 visible" 
-        : "opacity-0 -translate-y-2 invisible pointer-events-none"
+        ${
+          error
+            ? "opacity-100 translate-y-0 visible"
+            : "opacity-0 -translate-y-2 invisible pointer-events-none"
         }
         
         `}
-        >
-      {errorData}
-    </span>
-  </div>
-    )
-    ;
-  }
+      >
+        {errorData}
+      </span>
+    </div>
+  );
+};
 export default FormField;
--- frontend/app/interfaces/types.ts
+++ frontend/app/interfaces/types.ts
@@ -1,20 +1,24 @@
-import { FieldError, UseFormRegister } from "react-hook-form";
+import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+type DataSimple = {
+  email: string;
+  password: string;
+};
 
 export type FormData = {
   email: string;
   password: string;
-  confirmPassword?: string;
+  confirmPassword: string;
 };
 
-export type FormFieldProps = {
+export type FormFieldProps<T extends FieldValues> = {
   id: string;
   type: string;
   placeholder: string;
-  name: ValidFieldNames;
-  register: UseFormRegister<FormData>;
+  name: Path<T>;
+  register: UseFormRegister<T>;
   error: FieldError | undefined;
   valueAsNumber?: boolean;
 };
 
 export type ValidFieldNames = "email" | "password" | "confirmPassword";
-
