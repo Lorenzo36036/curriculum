1d712c351045bd36e0925e71a465b0a56fab5daf
Lorenzo Parra <alejandro36036@gmail.com>
2025-12-22 17:24:39 -0400

    feature: implementation section zod of  formContact
    implementation of section zod for form and refactor of schema and data

--- frontend/app/tools/Zod.ts
+++ frontend/app/tools/Zod.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const UserSchema = z
+export const RegisterData = z
   .object({
     username: z.string().min(2, { message: "El nombre es muy corto" }),
     email: z.string().email({ message: "Correo electrónico no válido" }),
@@ -15,10 +15,18 @@ export const UserSchema = z
     path: ["confirmPassword"],
   });
 
-export const LoginSchema = z.object({
+export const LoginData = z.object({
   email: z.string().email("Correo no válido"),
   password: z.string().min(1, "La contraseña es obligatoria"),
 });
 
-export type LoginData = z.infer<typeof LoginSchema>;
-export type FormData = z.infer<typeof UserSchema>;
+export const formContactData = z.object({
+  name: z.string().min(1, "El nombre es obligatorio"),
+  email: z.string().email("Correo no válido"),
+  subject: z.string().min(1, "El asunto es obligatorio"),
+  message: z.string().min(1, "El mensaje es obligatorio"),
+});
+
+export type FormContactSchema = z.infer<typeof formContactData>;
+export type LoginDataSchema = z.infer<typeof LoginData>;
+export type RegisterDataSchema = z.infer<typeof RegisterData>;
