822807f8a6663fee634b2ac43ac03ead8b32312b
Lorenzo Parra <alejandro36036@gmail.com>
2025-12-24 15:12:07 -0400

    feature: abjust of screen
    abjust of screen

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} w-screen h-screen antialiased`}
       >
         <div className="fixed w-screen z-50">
           <Navbar />
