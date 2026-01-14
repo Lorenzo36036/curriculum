f6748a473f2e1d8f2f692f05474e8c5dd29c4cab
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-13 23:43:28 -0400

    feature; creation of toasts
     creation of toasts for sucefully and Error

---
+++ frontend/app/components/toast/ToastSucefully.tsx
@@ -0,0 +1,65 @@
+function ToastSucefully({
+  text,
+  setShow,
+}: {
+  text: string;
+  setShow: (show: boolean) => void;
+}) {
+  return (
+    <div
+      id="toast-success"
+      className="absolute right-4 top-20 flex items-center  max-w-[320px] w-full p-4 text-body bg-green-700 rounded-base shadow-lg shadow-green-600 rounded-xl"
+      role="alert"
+    >
+      <div className="inline-flex items-center justify-center shrink-0 w-7 h-7 text-fg-success bg-green-900 rounded-xl border-black">
+        <svg
+          className="w-20 h-20"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 11.917 9.724 16.5 19 7.5"
+          />
+        </svg>
+        <span className="sr-only">Check icon</span>
+      </div>
+      <div className="ms-3 text-lg font-normal">{text}</div>
+      <button
+        onClick={() => setShow(false)}
+        type="button"
+        className="hover:scale-120 ms-auto flex items-center justify-center text-body hover:text-heading bg-transparent box-border border border-transparent hover:bg-neutral-secondary-medium  font-medium leading-5 rounded text-sm h-8 w-8 "
+        data-dismiss-target="#toast-success"
+        aria-label="Close"
+      >
+        <span className="sr-only">Close</span>
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18 17.94 6M18 18 6.06 6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default ToastSucefully;
