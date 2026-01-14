2babeefbd7c08d3b7faefb340532f4f0be2de102
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-13 23:42:27 -0400

    feature: implementations de endspoints fronted
     implementations de endspoints fronted and abjust

--- frontend/app/(pages)/login/page.tsx
+++ frontend/app/(pages)/login/page.tsx
@@ -1,27 +1,71 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
-import FormField from '@/app/components/input/FormField';
-import { LoginData, LoginDataSchema } from '@/app/tools/Zod';
+import loginPost from "@/app/api/loginPost";
+import FormField from "@/app/components/input/FormField";
+import Spiner from "@/app/components/Spiner";
+import ToastError from "@/app/components/toast/ToastError";
+import ToastSucefully from "@/app/components/toast/ToastSucefully";
+import { LoginData, LoginDataSchema } from "@/app/tools/Zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from 'react-hook-form';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
-  
-   const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<LoginDataSchema>({
-      resolver: zodResolver(LoginData),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginDataSchema>({
+    resolver: zodResolver(LoginData),
+  });
+  const [load, setLoad] = useState(false);
+  const [text, setText] = useState("");
+  const [show, setShow] = useState(false);
+  const [sucefullyToast, setSucefullyToast] = useState(false);
 
-    const onSubmitData = async (data: LoginDataSchema): Promise<void> => {
-      console.log("SUCCESS", data);
-    };
-  
+  const onSubmitData = async (data: LoginDataSchema): Promise<void> => {
+    setLoad(true);
+    try {
+      await loginPost(data);
+      setSucefullyToast(true);
+      setText("Login Exitoso");
+      setShow(true);
+    } catch (error: any) {
+      setSucefullyToast(false);
+      if (error.response.data.statusCode === 401) {
+        return setText("Contrasena incorrecta");
+      }
+
+      if (error.response.data.statusCode === 404) {
+        return setText("Usuario no existe");
+      }
+
+      setText("Ocurrio un error");
+    } finally {
+      setLoad(false);
+      setShow(true);
+      setTimeout(() => {
+        setShow(false);
+      }, 4000);
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmitData)} className="bg-white h-screen w-screen flex items-center justify-center">
-      <div className="w-full flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <form onSubmit={handleSubmit(onSubmitData)}>
+      <div
+        className={`${
+          show ? "opacity-100 translate-x-0" : "translate-x-full opacity-0 "
+        } right-0 w-100 fixed transition-all transition-discrete duration-700 ease-in-out animate-bounce  `}
+      >
+        {sucefullyToast ? (
+          <ToastSucefully text={text} setShow={setShow} />
+        ) : (
+          <ToastError text={text} setShow={setShow} />
+        )}
+      </div>
+
+      <div className="bg-white h-screen w-full flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full rounded-lg  md:mt-0 sm:max-w-xl xl:p-0 bg-gray-50 border-gray-200 border-2 shadow-2xl">
           <div className="w-full p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-3xl text-center ">
@@ -36,13 +80,13 @@ export default function Login() {
                   Email
                 </label>
                 <FormField
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    register={register}
-                    error={errors.email}
-                  />
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  register={register}
+                  error={errors.email}
+                />
               </div>
               <div>
                 <label
@@ -52,19 +96,20 @@ export default function Login() {
                   Contraseña
                 </label>
                 <FormField
-                    type="password"
-                    placeholder="Contraseña"
-                    name="password"
-                    register={register}
-                    error={errors.password}
-                    id={"password"}
-                  />
+                  type="password"
+                  placeholder="Contraseña"
+                  name="password"
+                  register={register}
+                  error={errors.password}
+                  id={"password"}
+                />
               </div>
               <button
+                disabled={load}
                 type="submit"
-                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4   font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                className="flex justify-center w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4   font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               >
-                Iniciar sesión 
+                {load ? <Spiner /> : "Crear cuenta"}
               </button>
               <p className="text-sm font-light text-gray-900 ">
                 No tienes cuenta?{" "}
@@ -80,6 +125,5 @@ export default function Login() {
         </div>
       </div>
     </form>
-  )
+  );
 }
-
