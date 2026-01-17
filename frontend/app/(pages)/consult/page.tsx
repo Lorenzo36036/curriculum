ca24a791199b84c13766d3920ab1074a29613319
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-17 00:01:59 -0400

    feature: redirect in consult
    redirect in consult

--- frontend/app/(pages)/consult/page.tsx
+++ frontend/app/(pages)/consult/page.tsx
@@ -40,9 +40,8 @@ export default function ChatView() {
         </div>
         
         <button 
-          // onClick={handleLogout} // Comentado por ahora
           className="hover:scale-125 px-4 py-1.5 text-lg tracking-wide   font-semibold text-red-600 hover:text-red-500 rounded-full transition-all active:scale-95"
-          onClick={() => redirect('/')}
+          onClick={() => {redirect('/')}}
        >
           Salir
         </button>
