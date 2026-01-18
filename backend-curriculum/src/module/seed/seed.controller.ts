a3a94c502f5c476fdf7cb14fec910417c1e450c1
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-18 15:22:39 -0400

    feature: seed segurity
    seed segurity

--- backend-curriculum/src/module/seed/seed.controller.ts
+++ backend-curriculum/src/module/seed/seed.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UnauthorizedException } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
 @Controller('seed')
@@ -6,7 +6,11 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  runSeed() {
+  runSeed(@Query('key') key: string) {
+    if (key !== process.env.SEED_KEY) {
+      throw new UnauthorizedException('❌ ¡Cini ki cikici! No tienes permiso.');
+    }
+
     return this.seedService.create();
   }
 }
